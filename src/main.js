47ccb973b74e8a241bd2dea55ee15b6cbfd44c2b
EmmanueleD <emmanuele.online@gmail.com>
2022-11-17 21:07:21 -0300

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import axios from "axios";
 
-createApp(App).use(router).mount('#app')
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "/node_modules/primeflex/primeflex.css";
+
+import "./assets/scss/style.scss";
+
+axios.defaults.baseURL = process.env.VUE_APP_API_BASE;
+axios.defaults.headers.common[
+  "Authorization"
+] = `Bearer ${process.env.VUE_APP_BEARER_DIRECTUS}`;
+
+import PrimeVueConfigurator from "./utils/PrimeVueConfigurator";
+
+const pinia = createPinia();
+const app = createApp(App);
+
+PrimeVueConfigurator.init(app);
+app.use(pinia);
+app.use(router);
+
+app.mount("#app");
